4b4e899426f7ce21397bdb213df677ac8ec4e138
MojtabaGhanbarii <49097922+MojtabaGhanbarii@users.noreply.github.com>
2019-05-01 04:32:31 -0700

    Update LibraryBook.java
    change //

--- src/main/java/com/myproject/library_app/LibraryBook.java
+++ src/main/java/com/myproject/library_app/LibraryBook.java
@@ -4,6 +4,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.*;
+//
 @NoArgsConstructor
 //@RequiredArgsConstructor
 @Entity // This tells Hibernate to make a table out of this class
